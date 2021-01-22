5fa4f0c3557012d3c6e850fe0404d3adf9b4d715
zigart <agustinzigart@gmail.com>
2021-01-22 17:00:24 -0300

    En proceso

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+'use strict'
+
+
+var menuCheck = document.querySelector("#menu__check");
+var color = document.querySelector("a");
+
+function desplegar() {
+
+    console.log('aca va la funcion');
+
+}
+
+function esconder() {
+    console.log("aca se esconde");
+}
+
+
+menuCheck.addEventListener('click', () => {
+    if (menuCheck.checked == true) {
+        console.log("falta la funcion");
+        desplegar();
+    } else {
+        esconder();
+    }
+})
+
+
+
+
+
+
+window.onscroll = function() {
+    var h2 = document.getElementById("sobreMi");
+    var string = "Agustín Zigart";
+    var str = string.split("");
+    console.log(h2.innerHTML);
+
+    if (window.scrollY >= 250 && h2.innerHTML == "") {
+        (function animate() {
+            str.length > 0 ? h2.innerHTML += str.shift() : clearTimeout(running);
+            var running = setTimeout(animate, 90);
+        })();
+
+    }
+    console.log("Vertical: " + window.scrollY);
+
+
+};
